912f4d740e3e16cc062d930d52dff52761c5b97d
Ashish Upadhyay <itsashishupadhyay@gmail.com>
2022-08-18 01:54:49 -0400

    Added a Download Button

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ function OpenLink(What) {
   }else if (What === "GitHub"){
     window.open("https://github.com/itsashishupadhyay", "_blank");
   }else if (What == "Email"){
-    console.log("email")
-    // window.open ("mailto:xyz@yourapplicationdomain.com","emailWindow");
+    window.open ("mailto:itsashishupadhyay+HeyAshish@gmail.com","emailWindow");
     window.location.href = "mailto:itsashishupadhyay+HeyAshish@gmail.com";
   }else{
     window.open( What, "_blank");
@@ -18,3 +17,23 @@ smoothScroll = function(elementId) {
 
   element.scrollIntoView({behavior: "smooth"});
 }
+
+
+$(function() {
+  var btn = $(".GrabResume");
+
+  btn.on("click", function() {
+    $(this).addClass('GrabResume-progress');
+    setTimeout(function() {
+      btn.addClass('GrabResume-fill')
+    }, 500);
+
+    setTimeout(function() {
+      btn.removeClass('GrabResume-fill')
+    }, 4100);
+
+    setTimeout(function() {
+      btn.addClass('GrabResume-complete')
+    }, 2200);
+  });
+})
